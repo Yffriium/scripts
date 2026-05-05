a6635512780474bbb24ab738bcc5cde3d68f802b
Yffriium <gbloxers@gmail.com>
2026-05-05 02:31:22 -0500

    setup pins now

--- script.py
+++ script.py
@@ -16,25 +16,6 @@ INHALE_DURATION = 4.0 # seconds to spend inhaling. can be a decimal
 EXHALE_DURATION = 8.0 # seconds to spend exhaling. can be a decimal
 STALL_DURATION = 0.1 # seconds to spend stalling. can be a decimal. the "stall" time is what happens when we're waiting to see if we should inhale/exhale again. should be relatively low, but not too low. too low results in inefficiencies. 0.1 is a good number.
 
-
-
-
-### DO NOT EDIT BELOW THIS LINE
-### ---------------------------------------------------------------
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 base_options = python.BaseOptions(model_asset_path="pose_landmarker.task")
 options = vision.PoseLandmarkerOptions(
     base_options=base_options,
@@ -45,51 +26,23 @@ options = vision.PoseLandmarkerOptions(
 detector = vision.PoseLandmarker.create_from_options(options)
 cap = cv2.VideoCapture(0)
 
-
 frame_id = 0
-# is_breathing = True
-# is_breathing_lock = threading.Lock()
 
 GPIO.setmode(GPIO.BCM)
+
 GPIO.setup(OUTPUT_PIN, GPIO.OUT)
+GPIO.setup(10, GPIO.OUT)
+GPIO.setup(22, GPIO.OUT)
 
 GPIO.output(OUTPUT_PIN, GPIO.LOW)
 
-# def breathe():
-#     global is_breathing
-#     was_breathing = False
-#     inhale = True
+def breathe():
+    while True:
+        GPIO.output(22, GPIO.LOW)
+        GPIO.output(10, GPIO.LOW)
 
-#     while True:
-#         local_breathing = False
-#         with is_breathing_lock:
-#             local_breathing = is_breathing
-
-#         if local_breathing:
-#             if not was_breathing:
-#                 inhale = True
-#             if inhale:
-#                 print("Inhaling")
-#                 GPIO.output(OUTPUT_PIN, GPIO.HIGH)
-#                 time.sleep(INHALE_DURATION)
-#                 inhale = False
-#             else:
-#                 print("Exhaling")
-#                 GPIO.output(OUTPUT_PIN, GPIO.LOW)
-#                 time.sleep(EXHALE_DURATION)
-#                 inhale = True
-
-#             was_breathing = True
-#         else:
-#             GPIO.output(OUTPUT_PIN, GPIO.LOW)
-#             was_breathing = False
-#             print(".")
-#             time.sleep(STALL_DURATION)
-
-
-# t = threading.Thread(target=breathe)
-# t.start()
-
+t = threading.Thread(target=breathe)
+t.start()
 
 def get_vid():
     frame = freenect.sync_get_video()[0]
@@ -110,23 +63,17 @@ def compute_angle(a, b, c):
     angle = np.degrees(np.arccos(np.clip(cos_angle, -1.0, 1.0)))
     return angle
 
-
 mp_pose = mp.solutions.pose
 try:
     while True:
         GPIO.output(OUTPUT_PIN, GPIO.LOW)
-        GPIO.output(22, GPIO.HIGH)
-        GPIO.output(10, GPIO.HIGH)
         rgb = get_vid()
         mp_image = mp.Image(image_format=mp.ImageFormat.SRGB, data=rgb)
         result = detector.detect_for_video(mp_image, frame_id)
         frame_id += 1
         #rgb = get_vid()
         #depth = get_depth()
-
         #results = pose.process(cv2.cvtColor(rgb, cv2.COLOR_BGR2RGB))
-
-        turn_on_pin = False
         if result.pose_landmarks:
             
             for person_id, pose in enumerate(result.pose_landmarks):
@@ -162,32 +109,15 @@ try:
                 if (left_angle < KNEE_ANGLE_THRESHOLD or right_angle < KNEE_ANGLE_THRESHOLD):
                     
                     GPIO.output(OUTPUT_PIN, GPIO.HIGH)
-                    GPIO.output(22, GPIO.HIGH)
-                    GPIO.output(10, GPIO.HIGH)
             
                     time.sleep(EXHALE_DURATION)
 
                     GPIO.output(OUTPUT_PIN, GPIO.LOW)
-                    GPIO.output(22, GPIO.HIGH)
-                    GPIO.output(10, GPIO.HIGH)
 
                     time.sleep(INHALE_DURATION)
-
-                    # turn_on_pin = True
-                    # print(f"P{person_id} enables")
             
         else:
             print("NO POSE")
-
-        # if turn_on_pin:
-        #     with is_breathing_lock:
-        #         is_breathing = True
-        #     #print("ON=======================")
-            
-        # else:
-        #     with is_breathing_lock:
-        #         is_breathing = False
-            #print("off-------")
         
 finally:
     GPIO.cleanup()
